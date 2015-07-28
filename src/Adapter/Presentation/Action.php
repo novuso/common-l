d48a5f314aa9dfe7e2ce2251beca2b282f11867b
John Nickell <email@johnnickell.com>
2015-07-28 09:44:50 -0500

    presentation

--- src/Adapter/Presentation/Action.php
+++ src/Adapter/Presentation/Action.php
@@ -3,17 +3,18 @@
 namespace Novuso\Common\Adapter\Presentation;
 
 use Exception;
+use Novuso\System\Type\Type;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * Action is the interface for an HTTP request handler
+ * Action is the base class for an HTTP request handler
  *
  * @copyright Copyright (c) 2015, Novuso. <http://novuso.com>
  * @license   http://opensource.org/licenses/MIT The MIT License
  * @author    John Nickell <email@johnnickell.com>
  * @version   0.0.0
  */
-interface Action
+abstract class Action
 {
     /**
      * Handles a request
@@ -24,5 +25,22 @@ interface Action
      *
      * @throws Exception When unable to handle the request
      */
-    public function handle(Request $request);
+    abstract public function handle(Request $request);
+
+    /**
+     * Creates a view for the current request
+     *
+     * @param Request    $request    The request
+     * @param mixed|null $data       The domain payload
+     * @param array      $parameters Template parameters
+     *
+     * @return View
+     */
+    public function view(Request $request, $data, array $parameters = [])
+    {
+        $action = Type::create($this);
+        $format = $request->attributes->get('_format');
+
+        return new View($action, $data, $format, $parameters);
+    }
 }
--- src/Adapter/Presentation/Responder.php
+++ src/Adapter/Presentation/Responder.php
@@ -3,17 +3,21 @@
 namespace Novuso\Common\Adapter\Presentation;
 
 use Exception;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 /**
- * Responder is the interface for an HTTP response formatter
+ * Responder is the base class for an HTTP response formatter
  *
  * @copyright Copyright (c) 2015, Novuso. <http://novuso.com>
  * @license   http://opensource.org/licenses/MIT The MIT License
  * @author    John Nickell <email@johnnickell.com>
  * @version   0.0.0
  */
-interface Responder
+abstract class Responder
 {
     /**
      * Formats a view into a response
@@ -24,5 +28,100 @@ interface Responder
      *
      * @throws Exception When unable to format a response
      */
-    public function format(View $view);
+    abstract public function format(View $view);
+
+    /**
+     * Formats a template path
+     *
+     * @param string $name      The template name
+     * @param string $format    The template format
+     * @param string $extension The template extension
+     *
+     * @return string
+     */
+    public function template($name, $format = 'html', $extension = 'twig')
+    {
+        return sprintf('%s.%s.%s', str_replace(':', '/', $name), $format, $extension);
+    }
+
+    /**
+     * Creates a JSON response
+     *
+     * @param mixed  $data    The response data
+     * @param int    $status  The status code
+     * @param array  $headers An array of response headers
+     *
+     * @return JsonResponse
+     */
+    public function jsonResponse($data, $status = Response::HTTP_OK, array $headers = [])
+    {
+        return new JsonResponse($data, $status, $headers);
+    }
+
+    /**
+     * Creates a file response
+     *
+     * Options and defaults:
+     *
+     *     $options = [
+     *         'public'             => true,
+     *         'contentDisposition' => null,
+     *         'autoEtag'           => false,
+     *         'autoLastModified'   => true
+     *     ];
+     *
+     * @param SplFileInfo|string $file    The file to stream
+     * @param int                $status  The status code
+     * @param array              $headers An array of response headers
+     * @param array              $options Additional options
+     *
+     * @return BinaryFileResponse
+     */
+    public function fileResponse($file, $status = Response::HTTP_OK, array $headers = [], array $options = [])
+    {
+        $options = array_merge([
+            'public'             => true,
+            'contentDisposition' => null,
+            'autoEtag'           => false,
+            'autoLastModified'   => true
+        ], $options);
+
+        return new BinaryFileResponse(
+            $file,
+            $status,
+            $headers,
+            $options['public'],
+            $options['contentDisposition'],
+            $options['autoEtag'],
+            $options['autoLastModified']
+        );
+    }
+
+    /**
+     * Creates a redirect response
+     *
+     * @param string $url     The URL to redirect to
+     * @param int    $status  The status code
+     * @param array  $headers An array of response headers
+     *
+     * @return RedirectResponse
+     */
+    public function redirectResponse($url, $status = Response::HTTP_FOUND, array $headers = [])
+    {
+        return new RedirectResponse($url, $status, $headers);
+    }
+
+    /**
+     * Creates a streamed response
+     *
+     * @param callable $callback A callback function to echo content
+     * @param int      $status   The status code
+     * @param array    $headers  An array of response headers
+     *
+     * @return StreamedResponse
+     */
+    public function streamResponse(callable $callback, $status = Response::HTTP_OK, array $headers = [])
+    {
+        return new StreamedResponse($callback, $status, $headers);
+    }
 }
